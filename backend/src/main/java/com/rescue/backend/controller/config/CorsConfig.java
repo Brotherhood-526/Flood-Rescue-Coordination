30afefb39c1de41206c8fd22c3aaf704f4e32890
TrisDuc <voductri88714@gmail.com>
2026-03-14 08:49:22 +0700

    Remove config

--- backend/src/main/java/com/rescue/backend/controller/config/CorsConfig.java
+++ backend/src/main/java/com/rescue/backend/controller/config/CorsConfig.java
@@ -1,10 +1,8 @@
 package com.rescue.backend.controller.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
